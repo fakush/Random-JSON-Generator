c72336466daf431b72bf4603e4dc5e834cdef91b
Facundo Creus <fcreus@gmail.com>
2021-01-29 19:29:32 -0300

    working version

--- script.js
+++ script.js
@@ -29,10 +29,10 @@ function showDataFields() {
     for (let i = 0; i < index+1; i++) {
         aux+=`
         <div class="row">
-            <div class="alert alert-danger col 3" role="alert">
+            <div class="alert alert-danger col-3" role="alert">
                 ${myJSON[i].key}
             </div>
-            <div class="alert alert-danger col 9" role="alert">
+            <div class="alert alert-danger col-9" role="alert">
                 ${myJSON[i].values}
             </div>
         </div>
@@ -46,7 +46,7 @@ function showDataFields() {
 function addFieldset(){
     let dataForArray = new ArrayDeDatos(document.getElementById(`inputKey`).value, document.getElementById(`inputValues`).value);
     myJSON.push(dataForArray);
-    document.getElementById("arrayRowContainer").innerHTML = newFieldset;
+    // document.getElementById("arrayRowContainer").innerHTML = newFieldset;
     document.getElementById("arrayDataContainer").innerHTML = showDataFields();
     index++;
 }
@@ -58,22 +58,55 @@ function removeFieldset(){
 //Get User values/filters
 
 //Creates the JSON file
+let outputArray = []; //This array will contain all the mixed data to stringify into JSON
+
 function generateJSON(){
-    console.log("comienza la magia");
-    let iteraciones = document.getElementById(`multiplier`).value;
-    for (let i = 0; i < iteraciones; i++) {
-        //Logica de armado del JSON
+    // console.log("comienza la magia");
+    if (myJSON.length > 0) {
+        let iteraciones = parseInt(document.getElementById(`multiplier`).value);
+        let auxOut = ``;
+        for (let i = 0; i < iteraciones; i++) {
+            let auxArray = []; //En este array se arma cada "entrada"
+            let setCounter = document.getElementById(`addIndex`).value;
+            if (setCounter != false) {
+                auxArray[0] = i;
+                for (let i = 0; i < myJSON.length; i++) { //Toma un valor random de cada array y lo añade al array de salida.
+                    let valorRandom = myJSON[i].values.split(",");
+                    auxArray[i+1] = valorRandom[Math.floor(Math.random() * auxArray.length)];
+                }
+            } else {
+                for (let i = 0; i < myJSON.length; i++) { //Toma un valor random de cada array y lo añade al array de salida.
+                    let valorRandom = myJSON[i].values.split(",");
+                    auxArray[i] = valorRandom[Math.floor(Math.random() * valorRandom.length)];
+                }
+            }
+            auxOut+=`
+                        <div class="alert alert-danger" role="alert">
+                            ${auxArray}
+                        </div>
+                        `;
+            // console.log(`auxArray: ${i}`);
+            // console.log(auxArray);
+            outputArray.push(auxArray);
+        }
+        document.getElementById("outputArray").innerHTML = auxOut;
+    } else {
+        document.getElementById("outputArray").innerHTML = "Todavía no hay valores";
     }
+    document.getElementById("jsonData").innerHTML = `
+                                                    <div class="alert alert-danger col-12" role="alert">
+                                                        ${JSON.stringify(outputArray)}
+                                                    </div>
+                                                    `;
 }
 
 //Borra todo
 function clearForm(){
-    let myJSON = [];
+    myJSON = [];
     document.getElementById("arrayRowContainer").innerHTML = newFieldset;
-}
-//Export Options
-function generateJSON(){
-    console.log("genetateJSON");
+    document.getElementById("arrayDataContainer").innerHTML = "";
+    document.getElementById("outputArray").innerHTML = "";
+    document.getElementById("jsonData").innerHTML = "";
 }
 
 //Here we go!
